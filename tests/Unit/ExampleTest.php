adea19da178ff04571c0fb8eec926c7bcbb8ba91
mdenitti <massimodn77@gmail.com>
2026-05-27 10:40:00 +0200

    intruducing a warning in a test to check ci/cd behaviour

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
@@ -12,5 +13,18 @@ class ExampleTest extends TestCase
     public function test_that_true_is_true(): void
     {
         $this->assertTrue(true);
+    }
+
+    #[DataProvider('warningDemoProvider')]
+    public function test_that_intentionally_triggers_a_warning_for_ci_demo(string $first): void
+    {
+        $this->assertSame('first-value', $first);
+    }
+
+    public static function warningDemoProvider(): array
+    {
+        return [
+            'mismatched-arguments' => ['first-value', 'extra-value'],
+        ];
     }
 }
